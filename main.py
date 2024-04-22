46d116e982e2e5ebe751f5e621023bab6cdf1724
Christopher McLean <71855991+Iago-7@users.noreply.github.com>
2024-04-22 11:44:51 -0700

    Added choices and goodbye to main.py.

--- main.py
+++ main.py
@@ -6,9 +6,38 @@ LOGO_5 = "/____/\\__,_/ .___/\\___/_/      \\____/_/ .___/_/ /_/\\___/_/\n"
 LOGO_6 = "        /_/                        /_/"
 LOGO = LOGO_1 + LOGO_2 + LOGO_3 + LOGO_4 + LOGO_5 + LOGO_6
 
-
 print("Hello fellow cryptologist! Welcome to...")
 print(LOGO)
-name = input("Please enter your first name. ")
-print(f"Glad to have you aboard {name}!")
-print("Type '0' to read about our latest features.")
+user_name = input("Please type your first name and hit enter. ")
+print(f"Glad to have you aboard {user_name}! Let's get started!")
+
+# ADD TRY/CATCH HERE FOR NON-NUMERICAL INPUT
+user_choice = int(input(f"{user_name}, please type the number corresponding to the option you'd like, and hit enter:\n"
+                        "0 - See our latest features\n"
+                        "1 - Encrypt a message with a Caesar Cipher\n"
+                        "2 - Encrypt a message with a Mirror Cipher\n"
+                        "3 - Encrypt a message with a Vigenère Cipher\n"
+                        "4 - Encrypt a message with a Date-shift Cipher\n"
+                        "5 - Exit\n"))
+
+while user_choice > 5 or user_choice < -1:
+    user_choice = int(
+        input(f"{user_name}, please type the number corresponding to the option you'd like, and hit enter:\n"
+              "0 - See our latest features\n"
+              "1 - Encrypt a message with a Caesar Cipher\n"
+              "2 - Encrypt a message with a Mirror Cipher\n"
+              "3 - Encrypt a message with a Vigenère Cipher\n"
+              "4 - Encrypt a message with a Date-shift Cipher\n"
+              "5 - Exit\n"))
+if user_choice == 0:
+    pass  # load latest features info
+elif user_choice == 1:
+    pass  # load Caesar Cipher
+elif user_choice == 2:
+    pass  # load Mirror Cipher
+elif user_choice == 3:
+    pass  # load Vigenère Cipher
+elif user_choice == 4:
+    pass  # load Date-shift Cipher
+elif user_choice == 5:
+    print("Thanks for stopping by {name}! We'll see you next time!")
