0d6e1435cd97ce1182b0515c6741563b2ed9a341
Christopher McLean <71855991+Iago-7@users.noreply.github.com>
2024-04-22 11:21:57 -0700

    Added welcome message to main.py.

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ LOGO_2 = "  / ___/__  ______  ___  _____   / ____(_)___  / /_  ___  _____\n"
 LOGO_3 = "  \\__ \\/ / / / __ \\/ _ \\/ ___/  / /   / / __ \\/ __ \\/ _ \\/ ___/\n"
 LOGO_4 = " ___/ / /_/ / /_/ /  __/ /     / /___/ / /_/ / / / /  __/ /\n"
 LOGO_5 = "/____/\\__,_/ .___/\\___/_/      \\____/_/ .___/_/ /_/\\___/_/\n"
-LOGO_6 = "        /_/                        /_/\n"
+LOGO_6 = "        /_/                        /_/"
 LOGO = LOGO_1 + LOGO_2 + LOGO_3 + LOGO_4 + LOGO_5 + LOGO_6
-WELCOME_MESSAGE = "Hello friend! Welcome to..."
 
-print(WELCOME_MESSAGE)
+
+print("Hello fellow cryptologist! Welcome to...")
 print(LOGO)
+name = input("Please enter your first name. ")
+print(f"Glad to have you aboard {name}!")
+print("Type '0' to read about our latest features.")
