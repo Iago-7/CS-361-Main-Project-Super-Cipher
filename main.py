78f886d68eb9f3274bcfd9f88fa62b45d16db36e
Christopher McLean <71855991+Iago-7@users.noreply.github.com>
2024-05-06 11:57:39 -0700

    main.py: Fixed logo offset.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ LOGO_2 = "  / ___/__  ______  ___  _____   / ____(_)___  / /_  ___  _____\n"
 LOGO_3 = "  \\__ \\/ / / / __ \\/ _ \\/ ___/  / /   / / __ \\/ __ \\/ _ \\/ ___/\n"
 LOGO_4 = " ___/ / /_/ / /_/ /  __/ /     / /___/ / /_/ / / / /  __/ /\n"
 LOGO_5 = "/____/\\__,_/ .___/\\___/_/      \\____/_/ .___/_/ /_/\\___/_/\n"
-LOGO_6 = "        /_/                        /_/"
+LOGO_6 = "          /_/                      /_/"
 LOGO = LOGO_1 + LOGO_2 + LOGO_3 + LOGO_4 + LOGO_5 + LOGO_6
 USER_CHOICE_MESSAGE = ("please type the number corresponding to the option you'd like, and hit enter:\n"
                        "0 - What is Super Cipher?\n"
